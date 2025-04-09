902d5d13b2abd2ce0f97b16251ec7cfa3fdb78cf
Shaina Talisay <talisayshaina8@gmail.com>
2025-04-09 18:49:32 +0800

    debug error [Shaina]

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -3,48 +3,33 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import Link from 'next/link';
 
-
 export default function Dropdown() {
   return (
     <Menu as="div" className="relative inline-block">
       <div>
         <MenuButton className="inline-flex w-full justify-center rounded-full px-3 text-sm font-semibold">
-            <img alt="" src="/assets/dropdown.svg"className="h-8 w-auto"/>
+          <img alt="" src="/assets/dropdown.svg" className="h-8 w-auto"/>
         </MenuButton>
       </div>
       <MenuItems
-        transition
         className="absolute w-full justify-center px-3 py-3 mt-2 rounded-full bg-black"
       >
         <div className="py-1">
-          <Link href="/">
-          <MenuItem>
-            <a
-              href="#"
-              className="inline-flex w-full rounded-full hover:bg-gray-100"
-            >
-              <img alt="" src="/assets/home.svg"className="h-8 w-auto"/>
-            </a>
-          </MenuItem></Link>
-          <Link href="/guestviewevents">
-          <MenuItem>
-            <a
-              href="/guestviewevents"
-              className="inline-flex w-full rounded-full hover:bg-gray-100"
-            >
-              <img alt="" src="/assets/feature_search.svg"className="h-8 w-auto"/>
-            </a>
-          </MenuItem></Link>
-          <form action="#" method="POST">
-            <MenuItem>
-              <a
-                href="#"    
-                className="inline-flex w-full rounded-full hover:bg-gray-100"
-              >
-                <img alt="" src="/assets/settings.svg" className="h-8 w-auto"/>
-              </a>
+          <Link href="/" passHref legacyBehavior>
+            <MenuItem as="a" className="inline-flex w-full rounded-full hover:bg-gray-100">
+              <img alt="" src="/assets/home.svg" className="h-8 w-auto"/>
             </MenuItem>
-          </form>
+          </Link>
+          <Link href="/guestviewevents" passHref legacyBehavior>
+            <MenuItem as="a" className="inline-flex w-full rounded-full hover:bg-gray-100">
+              <img alt="" src="/assets/feature_search.svg" className="h-8 w-auto"/>
+            </MenuItem>
+          </Link>
+          <Link href="/settings" passHref legacyBehavior>
+            <MenuItem as="a" className="inline-flex w-full rounded-full hover:bg-gray-100">
+              <img alt="" src="/assets/settings.svg" className="h-8 w-auto"/>
+            </MenuItem>
+          </Link>
         </div>
       </MenuItems>
     </Menu>
--- src/components/officersidebar.tsx
+++ src/components/officersidebar.tsx
@@ -17,54 +17,51 @@ const OfficerSidebar: React.FC = () => {
       <aside className="w-64 h-screen bg-gray-100 shadow-lg flex flex-col">
         {/* Sidebar Title with Logo */}
         <div className="p-6 bg-gray-100 flex justify-center items-center">
-          <Link href="/orgpage">
-            <img src="/assets/OMSLOGO.png" alt="OMS Logo" className="h-12 mt-4" />
+          <Link href="/orgpage" legacyBehavior>
+            <a>
+              <img src="/assets/OMSLOGO.png" alt="OMS Logo" className="h-12 mt-4" />
+            </a>
           </Link>
         </div>
 
         {/* Navigation */}
         <nav className="flex-grow mt-4">
-          <a
-            href="/orgpage"
-            className="flex items-center px-6 py-3 text-gray-600 hover:bg-purple-100 hover:text-purple-600 transition-colors"
-          >
-            <DashboardIcon />
-            <span className="ml-3 text-md font-medium">Dashboard</span>
-          </a>
+          <Link href="/orgpage" legacyBehavior>
+            <a className="flex items-center px-6 py-3 text-gray-600 hover:bg-purple-100 hover:text-purple-600 transition-colors">
+              <DashboardIcon />
+              <span className="ml-3 text-md font-medium">Dashboard</span>
+            </a>
+          </Link>
 
-          <a
-            href="/memberspageofficerview"
-            className="flex items-center px-6 py-3 text-gray-600 hover:bg-purple-100 hover:text-purple-600 transition-colors"
-          >
-            <GroupIcon />
-            <span className="ml-3 text-md font-medium">Members</span>
-          </a>
+          <Link href="/memberspageofficerview" legacyBehavior>
+            <a className="flex items-center px-6 py-3 text-gray-600 hover:bg-purple-100 hover:text-purple-600 transition-colors">
+              <GroupIcon />
+              <span className="ml-3 text-md font-medium">Members</span>
+            </a>
+          </Link>
 
-          <a
-            href="/events"
-            className="flex items-center px-6 py-3 text-gray-600 hover:bg-purple-100 hover:text-purple-600 transition-colors"
-          >
-            <EventIcon />
-            <span className="ml-3 text-md font-medium">All Events</span>
-          </a>
+          <Link href="/events" legacyBehavior>
+            <a className="flex items-center px-6 py-3 text-gray-600 hover:bg-purple-100 hover:text-purple-600 transition-colors">
+              <EventIcon />
+              <span className="ml-3 text-md font-medium">All Events</span>
+            </a>
+          </Link>
 
-          <a
-            href="/orgcalendarsidebar"
-            className="flex items-center px-6 py-3 text-gray-600 hover:bg-purple-100 hover:text-purple-600 transition-colors"
-          >
-            <CalendarTodayIcon />
-            <span className="ml-3 text-md font-medium">Calendar</span>
-          </a>
+          <Link href="/orgcalendarsidebar" legacyBehavior>
+            <a className="flex items-center px-6 py-3 text-gray-600 hover:bg-purple-100 hover:text-purple-600 transition-colors">
+              <CalendarTodayIcon />
+              <span className="ml-3 text-md font-medium">Calendar</span>
+            </a>
+          </Link>
 
           <hr className="my-4 border-gray-300" />
 
-          <a
-            href="/aboutuspage"
-            className="flex items-center px-6 py-3 text-gray-600 hover:bg-purple-100 hover:text-purple-600 transition-colors"
-          >
-            <InfoIcon />
-            <span className="ml-3 text-md font-medium">Information</span>
-          </a>
+          <Link href="/aboutuspage" legacyBehavior>
+            <a className="flex items-center px-6 py-3 text-gray-600 hover:bg-purple-100 hover:text-purple-600 transition-colors">
+              <InfoIcon />
+              <span className="ml-3 text-md font-medium">Information</span>
+            </a>
+          </Link>
 
           <button
             onClick={() => setShowProfileSettings(true)}
