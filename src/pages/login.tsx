a6cc18671fc39e74d544ca09e7ecfb9e8f3eef55
Don <esfpabua@gmail.com>
2025-05-21 00:06:49 +0800

    Improved background color for login and signup pages. Also fixed the height for register org form.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,6 +7,7 @@ import { getDoc, doc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "@fortawesome/fontawesome-free/css/all.min.css"; // Import FontAwesome CSS
 
 function Login() {
   const [email, setEmail] = useState<string>("");
@@ -75,87 +76,100 @@ function Login() {
   };
 
   return (
-    <div className="bg-gradient-to-br from-indigo-300 to-fuchsia-700 min-h-screen flex items-center justify-center">
-      <ToastContainer />
-      <div className="flex w-[60%] max-w-7xl bg-white shadow-md rounded-lg overflow-hidden">
-        {/* Left Side (Logo and Welcome) */}
-        <div className="w-1/2 bg-gradient-to-br from-gray-200 to-purple-500 text-white p-8 flex flex-col justify-center items-center">
-          <img src="/assets/OMSLOGO.png" alt="OMS Logo" className="h-32 mb-4" />
-          <h1 className="text-3xl font-bold mb-4 text-center">Welcome to OMS</h1>
-          <p className="text-lg text-center mb-8">Sign in to continue access.</p>
-          <div className="flex flex-col items-center">
-            <button
-              onClick={handleGoogleSignUp}
-              className="w-[105%] bg-red-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4 flex items-center justify-center"
-            >
-              <i className="fab fa-google mr-2"></i> Sign in with Google
-            </button>
-            <button
-              onClick={handleFacebookSignUp}
-              className="w-[105%] bg-blue-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center"
-            >
-              <i className="fab fa-facebook-f mr-2"></i> Sign in with Facebook
-            </button>
-          </div>
-          <p className="text-center mt-8 text-sm">www.organizationmanagementsystem.com</p>
-        </div>
-        {/* Right Side (Form) */}
-        <div className="w-1/2 p-8">
-          <h3 className="text-2xl font-bold text-center mb-6">SIGN IN</h3>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label
-                htmlFor="email"
-                className="block text-gray-700 font-medium mb-2"
-              >
-                Email address
-              </label>
-              <input
-                id="email"
-                type="email"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="password"
-                className="block text-gray-700 font-medium mb-2"
-              >
-                Password
-              </label>
-              <input
-                id="password"
-                type="password"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-                placeholder="Enter password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="d-grid">
-              <button
-                type="submit"
-                className="w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-          <p className="text-center mt-4">
-            New user?{" "}
-            <a href="/choose" className="text-purple-600 font-bold">
-              Register Here
-            </a>
+		<div className="bg-gradient-to-tr from-purple-400 via-fuchsia-500 to-indigo-700 min-h-screen flex items-center justify-center">
+			<ToastContainer />
+			<div className="flex w-[60%] max-w-7xl bg-white shadow-md rounded-xl overflow-hidden">
+				{/* Left Side (Logo and Welcome) */}
+				<div className="w-1/2 bg-gradient-to-tr from-purple-200 via-fuchsia-200 to-indigo-300 text-white p-8 flex flex-col justify-center items-center">
+					<img
+						src="/assets/OMSLOGO.png"
+						alt="OMS Logo"
+						className="h-32 mb-4"
+					/>
+					<h1 className="text-3xl font-bold mb-4 text-center text-purple-800 drop-shadow-lg">
+						Welcome to OMS
+					</h1>
+					<p className="text-lg text-center mb-8 text-purple-800 drop-shadow-lg">
+						Sign in to continue access.
+					</p>
+					<div className="flex flex-col items-center">
+						<button
+							onClick={handleGoogleSignUp}
+							className="w-[105%] bg-red-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4 flex items-center justify-center"
+						>
+							<i className="fab fa-google mr-2  "></i> Sign in with Google
+						</button>
+						<button
+							onClick={handleFacebookSignUp}
+							className="w-[105%] bg-blue-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center justify-center"
+						>
+							<i className="fab fa-facebook-f mr-2"></i> Sign in with Facebook
+						</button>
+					</div>
+          <p className="text-center mt-8 text-sm text-purple-900 drop-shadow-md">
+            www.organizationmanagementsystem.com
           </p>
-          <br />
-          <hr />
-        </div>
-      </div>
-    </div>
-  );
+				</div>
+				{/* Right Side (Form) */}
+				<div className="w-1/2 p-8">
+					<h3 className="text-2xl font-bold text-center mb-6">SIGN IN</h3>
+					<form onSubmit={handleSubmit}>
+						<div className="mb-3">
+							<label
+								htmlFor="email"
+								className="block text-gray-700 font-medium mb-2"
+							>
+								Email address
+							</label>
+							<input
+								id="email"
+								type="email"
+								className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
+								placeholder="Enter email"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+						</div>
+						<div className="mb-4">
+							<label
+								htmlFor="password"
+								className="block text-gray-700 font-medium mb-2"
+							>
+								Password
+							</label>
+							<input
+								id="password"
+								type="password"
+								className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
+								placeholder="Enter password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+						</div>
+						<div className="d-grid">
+							<button
+								type="submit"
+								className="w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+							>
+								Submit
+							</button>
+						</div>
+					</form>
+					<p className="text-center mt-4">
+						New user?{" "}
+						<a
+							href="/choose"
+							className="text-purple-600 font-bold"
+						>
+							Register Here
+						</a>
+					</p>
+					<br />
+					<hr />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
--- src/pages/registerorg.tsx
+++ src/pages/registerorg.tsx
@@ -169,176 +169,217 @@ function RegisterOrg() {
   };
 
   return (
-    <div className="bg-gradient-to-bl from-cyan-400 to-fuchsia-600 min-h-screen flex items-center justify-center shadow-lg">
-      <div className="flex flex-col md:flex-row w-[65%] max-w-6xl bg-white shadow-lg rounded-xl overflow-hidden">
+		<div className="bg-gradient-to-tr from-purple-400 via-fuchsia-500 to-indigo-700 min-h-screen flex items-center justify-center shadow-lg">
+			<div className="flex h-[650px] w-[65%] max-w-6xl bg-white shadow-lg rounded-xl overflow-hidden ">
+				{/* Left Side - Registration Form */}
+        <div className="w-full md:w-1/2 p-8 overflow-y-auto">
+					<h3 className="text-2xl font-bold text-center mb-6">
+						Register Organization
+					</h3>
 
-        {/* Left Side - Registration Form */}
-        <div className="w-full md:w-1/2 p-8">
-          <h3 className="text-2xl font-bold text-center mb-6">Register Organization</h3>
+					{errorMessage && (
+						<div className="bg-red-500 text-white p-3 rounded-md mb-4 text-center">
+							{errorMessage}
+						</div>
+					)}
+					{successMessage && (
+						<div className="bg-green-500 text-white p-3 rounded-md mb-4 text-center">
+							{successMessage}
+						</div>
+					)}
 
-          {errorMessage && (
-            <div className="bg-red-500 text-white p-3 rounded-md mb-4 text-center">
-              {errorMessage}
-            </div>
-          )}
-          {successMessage && (
-            <div className="bg-green-500 text-white p-3 rounded-md mb-4 text-center">
-              {successMessage}
-            </div>
-          )}
+					<form
+						onSubmit={handleRegister}
+						className="space-y-2"
+					>
+						<div>
+							<label
+								htmlFor="oname"
+								className="block text-gray-600 font-medium mb-1"
+							>
+								Organization Name
+							</label>
+							<input
+								type="text"
+								id="oname"
+								className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+								placeholder="Enter organization name"
+								onChange={(e) => setOname(e.target.value)}
+								required
+							/>
+						</div>
+						<div>
+							<label
+								htmlFor="description"
+								className="block text-gray-600 font-medium mb-1"
+							>
+								Organization Description
+							</label>
+							<textarea
+								id="description"
+								className="h-[50%] w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+								placeholder="Enter a short description of your organization"
+								onChange={(e) => setDescription(e.target.value)}
+								required
+							/>
+						</div>
+						<div>
+							<label
+								htmlFor="tags"
+								className="block text-gray-600 font-medium mb-1"
+							>
+								Select Organization Tags
+							</label>
+							<select
+								id="tags"
+								className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+								onChange={handleTagChange}
+								value={selectedTag}
+							>
+								<option value="">Select a tag</option>
+								<option value="sports">Sports</option>
+								<option value="academic">Academic</option>
+								<option value="interest">Interest</option>
+								<option value="other">Other (Specify)</option>
+							</select>
+							{selectedTag === "other" && (
+								<div className="mt-2">
+									<input
+										type="text"
+										placeholder="Enter your custom tag"
+										value={otherTag}
+										onChange={handleOtherTagChange}
+										className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+									/>
+								</div>
+							)}
+						</div>
 
-          <form onSubmit={handleRegister} className="space-y-2">
-            <div>
-              <label htmlFor="oname" className="block text-gray-600 font-medium mb-1">
-                Organization Name
-              </label>
-              <input
-                type="text"
-                id="oname"
-                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                placeholder="Enter organization name"
-                onChange={(e) => setOname(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="description" className="block text-gray-600 font-medium mb-1">
-                Organization Description
-              </label>
-              <textarea
-                id="description"
-                className="h-[50%] w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                placeholder="Enter a short description of your organization"
-                onChange={(e) => setDescription(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="tags" className="block text-gray-600 font-medium mb-1">
-                Select Organization Tags
-              </label>
-              <select
-                id="tags"
-                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                onChange={handleTagChange}
-                value={selectedTag}
-              >
-                <option value="">Select a tag</option>
-                <option value="sports">Sports</option>
-                <option value="academic">Academic</option>
-                <option value="interest">Interest</option>
-                <option value="other">Other (Specify)</option>
-              </select>
-              {selectedTag === "other" && (
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    placeholder="Enter your custom tag"
-                    value={otherTag}
-                    onChange={handleOtherTagChange}
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                  />
-                </div>
-              )}
-            </div>
+						<div>
+							<label
+								htmlFor="email"
+								className="block text-gray-600 font-medium mb-1"
+							>
+								Email
+							</label>
+							<input
+								type="email"
+								id="email"
+								className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+								placeholder="Enter email"
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+						</div>
+						<div>
+							<label
+								htmlFor="password"
+								className="block text-gray-600 font-medium mb-1"
+							>
+								Password
+							</label>
+							<input
+								type="password"
+								id="password"
+								className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+								placeholder="Enter password"
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+						</div>
 
-            <div>
-              <label htmlFor="email" className="block text-gray-600 font-medium mb-1">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                placeholder="Enter email"
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="block text-gray-600 font-medium mb-1">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                placeholder="Enter password"
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
+						<div className="flex items-center">
+							<div className="w-full md:w-2/3">
+								<label
+									htmlFor="logo"
+									className="block text-gray-600 font-medium mb-1"
+								>
+									Upload Logo
+								</label>
+								<div className="relative">
+									<input
+										type="file"
+										id="logo"
+										accept="image/*"
+										onChange={handleLogoChange}
+										className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 opacity-0"
+									/>
+									<label
+										htmlFor="logo"
+										className="text-purple-500 text-m font-bold underline cursor-pointer absolute top-0 left-0 w-full h-full flex w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none"
+									>
+										Choose Org Logo
+									</label>
+								</div>
+							</div>
+							<div className="w-24 h-24 rounded-full overflow-hidden ml-4 border-2 border-purple-500">
+								{logoPreview ? (
+									<img
+										src={logoPreview}
+										alt="Logo Preview"
+										className="w-full h-full object-cover"
+									/>
+								) : (
+									<div className="w-full h-full bg-gray-300 flex items-center justify-center">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											className="h-12 w-12 text-gray-500"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke="currentColor"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth={2}
+												d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+											/>
+										</svg>
+									</div>
+								)}
+							</div>
+						</div>
 
-            <div className="flex items-center">
-              <div className="w-full md:w-2/3">
-                <label htmlFor="logo" className="block text-gray-600 font-medium mb-1">
-                  Upload Logo
-                </label>
-                    <div className="relative"> 
-                    <input
-                        type="file"
-                        id="logo"
-                        accept="image/*"
-                        onChange={handleLogoChange}
-                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 opacity-0" 
-                    />
-                    <label htmlFor="logo" className="text-purple-500 text-m font-bold underline cursor-pointer absolute top-0 left-0 w-full h-full flex w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none"> 
-                        Choose Org Logo
-                    </label>
-                </div>
-              </div>
-              <div className="w-24 h-24 rounded-full overflow-hidden ml-4 border-2 border-purple-500">
-                {logoPreview ? (
-                  <img src={logoPreview} alt="Logo Preview" className="w-full h-full object-cover" />
-                ) : (
-                  <div className="w-full h-full bg-gray-300 flex items-center justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-12 w-12 text-gray-500"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                      />
-                    </svg>
-                  </div>
-                )}
-              </div>
-            </div>
+						<button
+							type="submit"
+							className="w-full py-2 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-md mt-4 hover:bg-blue-700 focus:outline-none"
+							disabled={loading}
+						>
+							{loading ? "Registering..." : "Register"}
+						</button>
+					</form>
+					<p className="text-center mt-4 text-gray-600">
+						Already have an account?{" "}
+						<a
+							href="/login"
+							className="text-purple-600 font-bold"
+						>
+							Login
+						</a>
+					</p>
+				</div>
 
-            <button
-              type="submit"
-              className="w-full py-2 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-md mt-4 hover:bg-blue-700 focus:outline-none"
-              disabled={loading}
-            >
-              {loading ? "Registering..." : "Register"}
-            </button>
-          </form>
-          <p className="text-center mt-4 text-gray-600">
-            Already have an account?{" "}
-            <a href="/login" className="text-purple-600 font-bold">
-              Login
-            </a>
-          </p>
-        </div>
-
-        {/* Right Side - OMS Welcome Section */}
-        <div className="w-full md:w-1/2 bg-gradient-to-bl from-cyan-200 to-fuchsia-500 text-white p-8 flex flex-col justify-center items-center">
-          <img src="./assets/OMSLOGO.png" alt="OMS Logo" className="h-32 mb-6" />
-          <h1 className="text-3xl font-extrabold mb-4 text-center">Welcome to OMS</h1>
-          <p className="text-lg text-center mb-6">
-            Register your organization and start managing it efficiently with us.
-          </p>
-          <p className="text-center mt-8 text-sm">www.organizationmanagementsystem.com</p>
-        </div>
-      </div>
-    </div>
-  );
+				{/* Right Side - OMS Welcome Section */}
+				<div className="w-full md:w-1/2 bg-gradient-to-tr from-purple-200 via-fuchsia-200 to-indigo-300 text-white p-8 flex flex-col justify-center items-center">
+					<img
+						src="./assets/OMSLOGO.png"
+						alt="OMS Logo"
+						className="h-32 mb-6"
+					/>
+					<h1 className="text-3xl font-extrabold mb-4 text-center text-purple-800 drop-shadow-lg">
+						Welcome to OMS
+					</h1>
+					<p className="text-lg text-center mb-6 text-purple-800 drop-shadow-lg">
+						Register your organization and start managing it efficiently with
+						us.
+					</p>
+					<p className="text-center mt-8 text-sm text-purple-800 drop-shadow-lg">
+						www.organizationmanagementsystem.com
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default RegisterOrg;
